daef8cb1ebe1d6b4c32c4b95bf37d9c7149c9794
Nicholas Wierenga <102650184+NicholasWierenga@users.noreply.github.com>
2022-07-18 03:15:14 -0400

    fixed ordering bug, added additional form and graph params

--- Grapher/ClientApp/src/app/grapher/grapher.component.ts
+++ Grapher/ClientApp/src/app/grapher/grapher.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GraphService } from '../graph.service';
 import { Point } from '../point';
 import { Chart, registerables } from 'chart.js';
-import { create, all } from 'mathjs';
+import { create, all, BigNumberDependencies } from 'mathjs';
 import { BigNumber } from "bignumber.js";
 
 @Component({
@@ -13,15 +13,17 @@ import { BigNumber } from "bignumber.js";
 
 export class GrapherComponent implements OnInit {
   equation: string = "";
-  xWindowLower: BigNumber = new BigNumber(0);
-  xWindowUpper: BigNumber = new BigNumber(12);
-  yWindowLower: BigNumber = new BigNumber(0); // These ywindows maybe should be deleted and have them decided
-  yWindowUpper: BigNumber = new BigNumber(100);
-  xSteps: BigNumber = new BigNumber(5); // Hardcoded currently, but intended to be adjustable by user.
+  xWindowLowerString: string = "";
+  xWindowUpperString: string = "";
+  xStepsString: string = "";
   pointsToGraph: Point[] = [];
   badEquation: boolean = false;
   math = create(all, {number: 'BigNumber'});
-  xStepDelta: BigNumber = new BigNumber(this.xWindowUpper.minus(this.xWindowLower).dividedBy(this.xSteps)); // to know how far to travel on x-axis before calculating new point
+  myChart: any;
+  //yWindowLowerString: string = "";
+  //yWindowUppeStringr: string = "";
+  // yWindows are not used currently, because ChartJS does them automatically.
+  // They could be used in the future, so they'll be left commented out.
   
   constructor(private graphService: GraphService) { 
     Chart.register(...registerables);
@@ -31,12 +33,13 @@ export class GrapherComponent implements OnInit {
   // This would function like below, but calculate one variable, then loop through all values for the other variable
   // before calculating the next point.
   getGraph(): void {
-    console.log("Number of steps taken for this: " + this.xSteps.valueOf());
-    console.log("Value of xStepDelta, the amount of distance crossed for each step: " + this.xStepDelta.valueOf());
-    
+    if (this.myChart instanceof Chart){ // We need to destroy the graph before we can display another.
+      this.myChart.destroy();
+    }
+
     // Line Chart
-    const lineCanvasEle: any = document.getElementById('myChart')
-    const myChart = new Chart(lineCanvasEle.getContext('2d'), {
+    const lineCanvasEle: any = document.getElementById('myChart');
+    this.myChart = new Chart(lineCanvasEle.getContext('2d'), {
       type: 'line',
         data: {
           labels: this.pointsToGraph.map(point => point.xcoord),
@@ -48,11 +51,14 @@ export class GrapherComponent implements OnInit {
           responsive: true,
           scales: {
             x: {
-              min: this.xWindowLower.toString(),
-              max: this.xWindowUpper.toString()
-            }
-
-            // If y-window are something that's wanted, it'd be here.
+              min: this.xWindowLowerString,
+              max: this.xWindowUpperString
+            },
+            // If yWindow adjustment is ever needed, below is what will be used.
+            //y: {
+            //  min: this.yWindowLower.toString(),
+            //  max: this.yWindowUpper.toString()
+            //}
           }
         }
     });
@@ -60,12 +66,18 @@ export class GrapherComponent implements OnInit {
   
   getPoints(): void {
     this.badEquation = false;
-    let currXVal: BigNumber = this.xWindowLower;
+    this.pointsToGraph = [];
     let newPoints: Point[] = [];
-    // TODO: Trying to graph another expression causes an error. Canvas must be destroyed first in order to do this.
-    // There's probably a function that does this on ChartJS.
+    let xWindowLower: BigNumber = new BigNumber(this.xWindowLowerString);
+    let xWindowUpper: BigNumber = new BigNumber(this.xWindowUpperString);
+    let xSteps: BigNumber = new BigNumber(this.xStepsString);
+    let currXVal: BigNumber = xWindowLower;
+    let xStepDelta: BigNumber = new BigNumber(xWindowUpper.minus(xWindowLower).dividedBy(xSteps)); // to know how far to travel on x-axis before calculating new point
 
-    if (this.equation === "") {
+    console.log("Number of steps for this graph: " + xSteps.valueOf());
+    console.log("Value of xStepDelta, the amount of distance crossed by each step: " + xStepDelta.valueOf());
+
+    if (this.equation.trim() === "") {
       this.badEquation = true;
       return;
     }
@@ -74,15 +86,15 @@ export class GrapherComponent implements OnInit {
     // something to remove duplicates in pointsToGraph later on.
     this.graphService.getPoints(this.equation).subscribe(points => { // Points is an array of all points from the DB with the current equation.
       this.pointsToGraph = points.filter(point => 
-        new BigNumber(point.xcoord).isGreaterThanOrEqualTo(this.xWindowLower) && new BigNumber(point.xcoord).isLessThanOrEqualTo(this.xWindowUpper) // filters for points in window range
-        && (new BigNumber(point.xcoord).minus(this.xWindowLower)).mod(this.xStepDelta).isEqualTo(new BigNumber(0)) // filters points that are in the set of stepped points.
+        new BigNumber(point.xcoord).isGreaterThanOrEqualTo(xWindowLower) && new BigNumber(point.xcoord).isLessThanOrEqualTo(xWindowUpper) // filters for points in window range
+        && (new BigNumber(point.xcoord).minus(xWindowLower)).mod(xStepDelta).isEqualTo(0) // filters points that are in the set of stepped points.
     );
+
     console.log(`Retrieved ${this.pointsToGraph.length} point(s) from the DB.`);
 
-
-    while (currXVal.isLessThanOrEqualTo(this.xWindowUpper)) {
+    while (currXVal.isLessThanOrEqualTo(xWindowUpper)) {
       if (this.pointsToGraph.some(point => point.xcoord === currXVal.toString())) { // We check if a point already exists here and skip the calculation if it does.
-        currXVal = currXVal.plus(this.xStepDelta);
+        currXVal = currXVal.plus(xStepDelta);
         continue;
       }
 
@@ -92,15 +104,33 @@ export class GrapherComponent implements OnInit {
       this.pointsToGraph.push(newPoint); // adds new point to the array
       newPoints.push(newPoint);
 
-      currXVal = currXVal.plus(this.xStepDelta);
+      currXVal = currXVal.plus(xStepDelta);
     }
 
-    this.graphService.createPoints(newPoints).subscribe(() => {
+    this.graphService.createPoints(newPoints).subscribe(() => { // Updates db with newly found points.
       console.log("Newly calculated points: " + newPoints.length);
 
+      // This sorting is needed to avoid previously calculated points from appearing at the start of the array,
+      // which causes those points to not be displayed by the ChartJS graph later on.
+      this.pointsToGraph.sort(function(pointA, pointB)  {
+        return new BigNumber(pointA.xcoord).minus(new BigNumber(pointB.xcoord)).s!
+        // .s gives the sign of the difference between the two points, so it'll give 1 or -1 as a number,
+        // which is returned in order to sort the array.
+      })
+
       this.getGraph();
-      }); // Updates db with newly found points.
+      });
     });
+  }
+
+  useTestValues(): void { // This is simply to avoid having to enter all parameters every time.
+  this.xWindowLowerString = "-10";
+  this.xWindowUpperString = "10";
+  this.xStepsString = "100";
+  //this.yWindowLower = new BigNumber(0);
+  //this.yWindowUpper = new BigNumber(100);
+
+  this.getPoints();
   }
 
   ngOnInit(): void {
